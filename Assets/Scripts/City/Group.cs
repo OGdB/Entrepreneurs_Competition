811210c9deaf93fcbca838c9651bfef62686d035
OGdB <58260499+OGdB@users.noreply.github.com>
2023-01-05 22:44:36 +0100

    Scripts and folders cleanup

--- Assets/Scripts/City/Group.cs
+++ Assets/Scripts/City/Group.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// Class representing and handling a Group's building
+/// </summary>
 public class Group : MonoBehaviour
 {
     private int groupNumber;
---
+++ Assets/Scripts/Managers & Singletons/DeveloperCommentSingleton.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class DeveloperCommentSingleton : MonoBehaviour
+{
+    public static DeveloperCommentSingleton Singleton;
+
+    public Canvas Canvas { get => canvas; set => canvas = value; }
+    private Canvas canvas;
+
+    public TMPro.TextMeshProUGUI developerText;
+
+    private void Awake()
+    {
+        if (!Singleton)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        Canvas = GetComponent<Canvas>();
+    }
+    public static void OnTriggerEntered()
+    {
+        Singleton.canvas.enabled = true;
+    }
+    public static void OnTriggerExited()
+    {
+        Singleton.canvas.enabled = false;
+    }
+    public static void SetDeveloperComment(string comment)
+    {
+        Singleton.developerText.SetText(comment);
+    }
+}
