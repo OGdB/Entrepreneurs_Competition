75cfcdb7cce5bd36c485e96097f22307b9aa0e53
OGdB <58260499+OGdB@users.noreply.github.com>
2023-01-05 16:09:45 +0100

    Quiz countdown and scene transition when all players pressed ready

---
+++ Assets/QuizCountdown.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuizCountdown : MonoBehaviour
+{
+    [SerializeField] 
+    private int countdownLength = 5;
+    [SerializeField] 
+    private TMPro.TextMeshProUGUI secondsLeftText;
+
+    private int secondsLeft;
+    private WaitForSeconds second;
+    private Coroutine countdownRoutine;
+
+    [SerializeField]
+    private GameObject UI;
+
+    private void Awake()
+    {
+        UI.SetActive(false);
+        second = new(1);
+    }
+    private void OnEnable() => DBManager.OnPressedReady += OnUsersReady;
+    private void OnDisable() => DBManager.OnPressedReady -= OnUsersReady;
+
+    private void OnUsersReady(bool allReady)
+    {
+        if (allReady && countdownRoutine == null)
+        {
+            UI.SetActive(true);
+            countdownRoutine = StartCoroutine(QuizCountdownCR());
+        }
+    }
+
+    private IEnumerator QuizCountdownCR()
+    {
+        secondsLeft = countdownLength;
+        secondsLeftText.SetText(secondsLeft.ToString());
+
+        while (secondsLeft > 0)
+        {
+            yield return second;
+            secondsLeft--;
+            secondsLeftText.SetText(secondsLeft.ToString());
+        }
+
+        SceneManager.LoadScene("Quiz");
+    }
+}
